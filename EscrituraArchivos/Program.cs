d94289a596004e7f580a9fcc80581d4b9a05a372
VilladaEdwinMSTR <93624470+VilladaEdwinMSTR@users.noreply.github.com>
2022-06-01 11:33:07 -0700

    Ejercicio en clase

--- EscrituraArchivos/Program.cs
+++ EscrituraArchivos/Program.cs
@@ -12,7 +12,7 @@ namespace EscrituraArchivos
     {
         static void Main(string[] args)
         {
-            StreamWriter sw = new StreamWriter("ejemplo.txt", true);
+            StreamWriter sw = new StreamWriter("ejemplo.txt");
             //Si el archivo no existe, lo creara
             //SI el archivo existe, escribira en el
 
@@ -20,11 +20,18 @@ namespace EscrituraArchivos
             "Esto es la segunda linea",
             "Esto es la tercer linea"};
 
+            //Ciclo para almacenar en arreglo
+            for(int i = 0; i < lines.Length; i++)
+            {
+                Console.Write("Ingrese su nombre: ");
+                lines[i] = Console.ReadLine();
+            }
+
             //Recorrer el arreglo para escribir las lineas en el archivo
             foreach(string line in lines) 
             {
                 //Escribir en el archivo
-                sw.WriteLine(line);
+                sw.WriteLine(line);               
             }
 
             //Cerrar el archivo
