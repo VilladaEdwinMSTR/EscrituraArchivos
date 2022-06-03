d53a7226ce0e6cf03f08a5e0dbc13cbb2abae0d8
VilladaEdwinMSTR <93624470+VilladaEdwinMSTR@users.noreply.github.com>
2022-06-02 20:32:16 -0700

    Tarea 02/06/2022

--- EscrituraArchivos/Program.cs
+++ EscrituraArchivos/Program.cs
@@ -10,35 +10,94 @@ namespace EscrituraArchivos
 {
     class Program
     {
+        //Clase Persona
+        public class Persona
+        {
+            //Campos de la clase
+            public string nombre;
+            public byte edad;
+
+            //Metodo constructor
+            public Persona(string nombre, byte edad)
+            {
+                //Diferenciar campos de parametros
+                this.nombre = nombre;
+                this.edad = edad;
+            }
+        }
         static void Main(string[] args)
         {
-            StreamWriter sw = new StreamWriter("ejemplo.txt");
-            //Si el archivo no existe, lo creara
-            //SI el archivo existe, escribira en el
+            //Variable booleana para salir de ciclo
+            bool x = false;
+            bool y = false;
 
-            string[] lines = {"Esto es la primer linea",
-            "Esto es la segunda linea",
-            "Esto es la tercer linea"};
-
-            //Ciclo para almacenar en arreglo
-            for(int i = 0; i < lines.Length; i++)
+            //Ciclo para repetir el mensaje
+            do
             {
-                Console.Write("Ingrese su nombre: ");
-                lines[i] = Console.ReadLine();
-            }
+                //Limpieza de pantalla
+                Console.Clear();
+                try
+                {
+                    //Despliegue de mensaje
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("\t\tCAPTURA DE DATOS\n");
+                    Console.ResetColor();
 
-            //Recorrer el arreglo para escribir las lineas en el archivo
-            foreach(string line in lines) 
-            {
-                //Escribir en el archivo
-                sw.WriteLine(line);               
-            }
+                    //Captura de datos
+                    Console.Write("Ingrese su nombre:");
+                    string nombre = Console.ReadLine();
 
-            //Cerrar el archivo
-            sw.Close();
+                    Console.Write("Ingrese su edad: ");
+                    byte edad = byte.Parse(Console.ReadLine());
 
-            Console.WriteLine("Escribiendo . . . . ");
-            Console.ReadKey();
+                    //Variable en true para salir de ciclo
+                    x = true;
+
+                        //Creacion de objeto
+                        Persona persona = new Persona(nombre, edad);
+
+                        StreamWriter sw = new StreamWriter("ejemplo.txt", true);
+                        //Si el archivo no existe, lo creara
+                        //SI el archivo existe, escribira en el
+
+                    //Escritura de texto en archivo
+                        sw.WriteLine(persona.nombre + " " + persona.edad);
+
+                        //Cerrar el archivo
+                        sw.Close();
+
+                    //Despliegue de mensaje
+                        Console.WriteLine("Escribiendo . . . . ");
+                        Console.ReadKey();                   
+                }
+                //Excepcion de formato
+                catch (FormatException)
+                {
+                    //Impresion de mensaje de color rojo
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("No se ingreso el formato correcto");
+                    Console.ResetColor();
+                }
+                //Mensaje para continuar
+                Console.Write("Presione ENTER para continuar . . . ");
+                Console.ReadKey();
+            } while (x==false); //fin de ciclo
         }
+        //string[] lines = {"Esto es la primer linea",
+        //"Esto es la segunda linea",
+        //"Esto es la tercer linea"};
+
+        //Ciclo para almacenar en arreglo
+        //for (int i = 0; i < lines.Length; i++)
+        //{
+        //    Console.Write("Ingrese su nombre: ");
+        //    lines[i] = Console.ReadLine();
+        //}
+        //Recorrer el arreglo para escribir las lineas en el archivo
+        //foreach (string line in lines)
+        //{
+        //    //Escribir en el archivo
+        //    sw.WriteLine(line);
+        //}
     }
 }
